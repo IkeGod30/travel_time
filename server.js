45fdfaa9793964819eec16727702c0b736e31759
Tony Izuogu <anthoizu@yahoo.com>
2023-09-10 19:35:48 -0500

    Updated cors configuration

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const connect = mongoose.connect(process.env.MONGO_URI,
   .then(() => console.log('MongoDB Connected now...'))
   .catch(err => console.log(err));
 
-app.use(cors())
+app.use(cors({origin:'*'}))
 
 
 //to not get any deprecation warning or error
