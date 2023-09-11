b4a18e4dd329c383500a50269e20b29e2ab5603a
Tony Izuogu <anthoizu@yahoo.com>
2023-09-10 20:22:27 -0500

    Updated index.js files

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const mongoose = require("mongoose");
 
 const connect = mongoose.connect(process.env.MONGODB_URI,
   {
-    useNewUrlParser: true, useUnifiedTopology: true
+    useNewUrlParser: true, useUnifiedTopology: true, dbName:'test'
     // useCreateIndex: true, useFindAndModify: false
   })
   .then(() => console.log('MongoDB Connected now...'))
